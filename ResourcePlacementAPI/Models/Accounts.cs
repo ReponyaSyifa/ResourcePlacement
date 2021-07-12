6df2d73270cf7b1e5c3de73568adc344652cf9bd
Mahe <mahendrapriyadi@gmail.com>
2021-07-12 10:56:33 +0700

    Add Accounts Model

---
+++ ResourcePlacementAPI/Models/Accounts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourcePlacementAPI.Models
+{
+    public class Accounts
+    {
+        [Key]
+        public int AccountId { get; set; }
+        public string Password { get; set; }
+        public string email { get; set; }
+    }
+}
