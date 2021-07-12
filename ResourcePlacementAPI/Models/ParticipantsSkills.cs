091ebb15f7cc3d37c70bd32e844036391cd6979e
Mahe <mahendrapriyadi@gmail.com>
2021-07-12 11:32:12 +0700

    Add Model for Many to Many Relation
    add some models : AccountsRoles, ParticipantsSkills, ProjectSkills

---
+++ ResourcePlacementAPI/Models/ParticipantsSkills.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourcePlacementAPI.Models
+{
+    public class ParticipantsSkills
+    {
+        public int SkillId { get; set; }
+        public int ParticipantId { get; set; }
+    }
+}
