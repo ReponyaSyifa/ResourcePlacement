3135a528619836d0c86db93b50da9c52127b079f
Mahe <mahendrapriyadi@gmail.com>
2021-07-12 11:16:05 +0700

    Update Table Name Accounts and CustomerUsers

--- ResourcePlacementAPI/Models/CustomerUsers.cs
+++ ResourcePlacementAPI/Models/CustomerUsers.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ResourcePlacementAPI.Models
 {
+    [Table("tb_M_CustomerUsers")]
     public class CustomerUsers
     {
         [Key]
