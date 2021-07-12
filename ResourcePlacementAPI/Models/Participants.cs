47827672ab2dbd565188378899d0ac7ec2ab14b1
Syifa <85871262+ReponyaSyifa@users.noreply.github.com>
2021-07-12 10:52:33 +0700

    add model: participant

---
+++ ResourcePlacementAPI/Models/Participants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourcePlacementAPI.Models
+{
+    public class Participants
+    {
+        [Key]
+        public int ParticipantId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Grade { get; set; }
+        public string Status { get; set; }
+
+    }
+    public enum Gender
+    {
+        Pria, Wanita
+    }
+}
