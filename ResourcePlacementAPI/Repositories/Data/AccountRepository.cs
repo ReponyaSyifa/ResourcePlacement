a4da024c46294fbaff7b50f365d69f0ac0e87ceb
Syifa <85871262+ReponyaSyifa@users.noreply.github.com>
2021-07-13 14:45:29 +0700

    Fix Login Method & Add ResetPasswordVM

--- ResourcePlacementAPI/Repositories/Data/AccountRepository.cs
+++ ResourcePlacementAPI/Repositories/Data/AccountRepository.cs
@@ -20,28 +20,44 @@ namespace ResourcePlacementAPI.Repositories.Data
         {
             var findAkun = myContext.Accounts.Find(loginVM.Email);
 
-            var matchAcc = myContext.Accounts.FirstOrDefault(a => a.Email == loginVM.Email && a.Password == loginVM.Password);
+            var matchAcc = myContext.Accounts.FirstOrDefault(a => a.Email == loginVM.Email);
+            var matchPass = myContext.Accounts.FirstOrDefault(p => p.Password == loginVM.Password);
             if (findAkun != null)
             {
-                if (matchAcc != null)
+                if (matchAcc != null && matchPass != null)
                 {
-                    if (HashingPassword.ValidatePassword(loginVM.Password, matchAcc.Password))
-                    {
-                        return 3;
-                    }
                     return 2;
                 }
                 return 1;
             }
             return 0;
         }
-        public string Guid() //
+        /*public string Guid() //
         {
             System.Guid guid = System.Guid.NewGuid();
             string newguid = guid.ToString();
             return newguid;
-        }
+        }*/
 
+        /*public int ResetPassword(ResetPasswordVM resetPwd)
+        {
+            System.Guid guid = System.Guid.NewGuid();
+            string newguid = guid.ToString();
 
+            var acc = new Accounts();
+
+            var accMatchEmp = myContext.Employees.FirstOrDefault(e => e.Email == resetPwd.Email);
+            var accMatchCU = myContext.CustomerUsers.FirstOrDefault(c => c.Email == resetPwd.Email);
+
+            if (accMatchEmp  != null)
+            {
+                acc.Email = accMatchEmp.Email;
+                acc.Password = HashingPassword.HashPassword(newguid);
+                myContext.Entry(accMatchEmp).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                var update = myContext.SaveChanges();
+
+                using (MailMessage)
+            }
+        }*/
     }
 }
