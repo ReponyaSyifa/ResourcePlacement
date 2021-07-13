7c5e821d20d1606ddb9858ade4a2d225af1fe7dd
Mahe <mahendrapriyadi@gmail.com>
2021-07-13 16:19:30 +0700

    Revert "Update Register"
    This reverts commit 1ee2c4689a5bb96a8a5f39f36cd714b03a65baf8.

--- ResourcePlacementAPI/Controllers/EmployeeController.cs
+++ ResourcePlacementAPI/Controllers/EmployeeController.cs
@@ -3,11 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using ResourcePlacementAPI.Base;
 using ResourcePlacementAPI.Models;
 using ResourcePlacementAPI.Repositories.Data;
-using ResourcePlacementAPI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace ResourcePlacementAPI.Controllers
@@ -16,36 +14,10 @@ namespace ResourcePlacementAPI.Controllers
     [ApiController]
     public class EmployeeController : BaseController<Employees, EmployeeRepository, int>
     {
-        private readonly EmployeeRepository repository;
+        private EmployeeRepository repository;
         public EmployeeController(EmployeeRepository repository) : base(repository)
         {
             this.repository = repository;
-        }
-
-        [HttpPost("Register")]
-        public ActionResult Register(RegisterVM registerVM)
-        {
-            var register = repository.RegisterRepo(registerVM);
-            if (register == 1)
-            {
-                return Ok(new { status = HttpStatusCode.OK, result = register, message = "succes" });
-            }
-            else if (register == 2)
-            {
-                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "Tidak ada data" });
-            }
-            else if (register == 3)
-            {
-                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "email sudah ada" });
-            }
-            else if (register == 4)
-            {
-                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "nik dan email sudah ada" });
-            }
-            else
-            {
-                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "Gagal menyimpan, data harus diisi semua" });
-            }
         }
     }
 }
