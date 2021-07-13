1ee2c4689a5bb96a8a5f39f36cd714b03a65baf8
Mahe <mahendrapriyadi@gmail.com>
2021-07-13 16:10:19 +0700

    Update Register

--- ResourcePlacementAPI/Controllers/EmployeeController.cs
+++ ResourcePlacementAPI/Controllers/EmployeeController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using ResourcePlacementAPI.Base;
 using ResourcePlacementAPI.Models;
 using ResourcePlacementAPI.Repositories.Data;
+using ResourcePlacementAPI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ResourcePlacementAPI.Controllers
@@ -14,10 +16,36 @@ namespace ResourcePlacementAPI.Controllers
     [ApiController]
     public class EmployeeController : BaseController<Employees, EmployeeRepository, int>
     {
-        private EmployeeRepository repository;
+        private readonly EmployeeRepository repository;
         public EmployeeController(EmployeeRepository repository) : base(repository)
         {
             this.repository = repository;
+        }
+
+        [HttpPost("Register")]
+        public ActionResult Register(RegisterVM registerVM)
+        {
+            var register = repository.RegisterRepo(registerVM);
+            if (register == 1)
+            {
+                return Ok(new { status = HttpStatusCode.OK, result = register, message = "succes" });
+            }
+            else if (register == 2)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "Tidak ada data" });
+            }
+            else if (register == 3)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "email sudah ada" });
+            }
+            else if (register == 4)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "nik dan email sudah ada" });
+            }
+            else
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "Gagal menyimpan, data harus diisi semua" });
+            }
         }
     }
 }
--- ResourcePlacementAPI/ViewModel/RegisterVM.cs
+++ ResourcePlacementAPI/ViewModel/RegisterVM.cs
@@ -7,5 +7,19 @@ namespace ResourcePlacementAPI.ViewModel
 {
     public class RegisterVM
     {
+        // register untuk Trainer Dan ADD2
+        public string NIK { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //register untuk User
+        public string CompanyName { get; set; }
+        public string PicName { get; set; }
+
+        // wajib ada di Trainer ADD2 dan User
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public int RoleId { get; set; }
     }
 }
