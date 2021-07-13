20405955dc76545dac09abd27a6d395b3b27fec0
Syifa <85871262+ReponyaSyifa@users.noreply.github.com>
2021-07-13 14:15:01 +0700

    Add Login in AccountRepo & HashingPassword

--- ResourcePlacementAPI/Repositories/Data/AccountRepository.cs
+++ ResourcePlacementAPI/Repositories/Data/AccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ResourcePlacementAPI.Context;
 using ResourcePlacementAPI.Models;
+using ResourcePlacementAPI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,38 @@ namespace ResourcePlacementAPI.Repositories.Data
 {
     public class AccountRepository : GeneralRepository<MyContext, Accounts, int>
     {
+        private readonly MyContext myContext;
         public AccountRepository(MyContext myContext) : base(myContext)
         {
-
+            this.myContext = myContext;
         }
+
+        public int Login(LoginVM loginVM)
+        {
+            var findAkun = myContext.Accounts.Find(loginVM.Email);
+
+            var matchAcc = myContext.Accounts.FirstOrDefault(a => a.Email == loginVM.Email && a.Password == loginVM.Password);
+            if (findAkun != null)
+            {
+                if (matchAcc != null)
+                {
+                    if (HashingPassword.ValidatePassword(loginVM.Password, matchAcc.Password))
+                    {
+                        return 3;
+                    }
+                    return 2;
+                }
+                return 1;
+            }
+            return 0;
+        }
+        public string Guid() //
+        {
+            System.Guid guid = System.Guid.NewGuid();
+            string newguid = guid.ToString();
+            return newguid;
+        }
+
+
     }
 }
--- ResourcePlacementAPI/ViewModel/LoginVM.cs
+++ ResourcePlacementAPI/ViewModel/LoginVM.cs
@@ -7,5 +7,7 @@ namespace ResourcePlacementAPI.ViewModel
 {
     public class LoginVM
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
