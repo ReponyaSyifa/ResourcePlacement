9944623f46e88afadf6eda34375c72e76eb52c59
Mahe <mahendrapriyadi@gmail.com>
2021-07-13 16:45:36 +0700

    Update Register

--- ResourcePlacementAPI/Controllers/EmployeeController.cs
+++ ResourcePlacementAPI/Controllers/EmployeeController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using ResourcePlacementAPI.Base;
 using ResourcePlacementAPI.Models;
 using ResourcePlacementAPI.Repositories.Data;
+using ResourcePlacementAPI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ResourcePlacementAPI.Controllers
@@ -18,6 +20,28 @@ namespace ResourcePlacementAPI.Controllers
         public EmployeeController(EmployeeRepository repository) : base(repository)
         {
             this.repository = repository;
+        }
+
+        [HttpPost("Register")]
+        public ActionResult Register(RegisterVM registerVM)
+        {
+            var register = repository.RegisterRepo(registerVM);
+            if (register == 1)
+            {
+                return Ok(new { status = HttpStatusCode.OK, result = register, message = "succes" });
+            }
+            else if (register == 2)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "nik sudah ada" });
+            }
+            else if (register == 3)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "data kurang" });
+            }
+            else
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, result = register, message = "Gagal menyimpan, data harus diisi semua" });
+            }
         }
     }
 }
--- ResourcePlacementAPI/Repositories/Data/EmployeeRepository.cs
+++ ResourcePlacementAPI/Repositories/Data/EmployeeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ResourcePlacementAPI.Context;
 using ResourcePlacementAPI.Models;
+using ResourcePlacementAPI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,112 @@ namespace ResourcePlacementAPI.Repositories.Data
 {
     public class EmployeeRepository : GeneralRepository<MyContext, Employees, int>
     {
+        private readonly MyContext myContext;
         public EmployeeRepository(MyContext myContext) : base(myContext)
         {
+            this.myContext = myContext;
+        }
 
+        public int RegisterRepo(RegisterVM registerVM)
+        {
+
+            var hashPass = HashingPassword.HashPassword(registerVM.Password);
+            var cekemail = myContext.Accounts.FirstOrDefault(e => e.Email == registerVM.Email);
+            if (cekemail == null) // gak ada yang sama
+            {
+                if (registerVM.PicName != null && registerVM.CompanyName != null)
+                {
+                    CustomerUsers customer = new CustomerUsers
+                    {
+                        Email = registerVM.Email,
+                        CompanyName = registerVM.CompanyName,
+                        PicName = registerVM.PicName
+                    };
+                    myContext.CustomerUsers.Add(customer);
+                    myContext.SaveChanges();
+
+                    // input untuk employee
+                    Employees employee = new Employees
+                    {
+                        NIK = "",
+                        FirstName = "",
+                        LastName = "",
+                        Email = ""
+                    };
+                    myContext.Employees.Add(employee);
+                    myContext.SaveChanges();
+
+                    Accounts accounts = new Accounts
+                    {
+                        EmployeeId = employee.EmployeeId,
+                        CustomerUserId = customer.CustomerUserId,
+                        Email = registerVM.Email,
+                        Password = hashPass
+                    };
+                    myContext.Accounts.Add(accounts);
+                    myContext.SaveChanges();
+
+                    AccountsRoles accountsRoles = new AccountsRoles
+                    {
+                        AccountId = accounts.AccountId,
+                        RolesId = registerVM.RoleId
+                    };
+                    myContext.AccountsRoles.Add(accountsRoles);
+                    myContext.SaveChanges();
+
+                    myContext.SaveChanges();
+                    return 1;
+                }
+                else if (registerVM.NIK != null && registerVM.FirstName != null && registerVM.LastName != null)
+                {
+                    // input untuk employee
+                    Employees employee = new Employees
+                    {
+                        NIK = registerVM.NIK,
+                        FirstName = registerVM.FirstName,
+                        LastName = registerVM.LastName,
+                        Email = registerVM.Email
+                    };
+                    myContext.Employees.Add(employee);
+                    myContext.SaveChanges();
+
+                    CustomerUsers customer = new CustomerUsers
+                    {
+                        Email = "",
+                        CompanyName = "",
+                        PicName = ""
+                    };
+                    myContext.CustomerUsers.Add(customer);
+                    myContext.SaveChanges();
+
+                    Accounts accounts = new Accounts
+                    {
+                        EmployeeId = employee.EmployeeId,
+                        CustomerUserId = customer.CustomerUserId,
+                        Email = registerVM.Email,
+                        Password = hashPass
+                    };
+                    myContext.Accounts.Add(accounts);
+                    myContext.SaveChanges();
+
+                    AccountsRoles accountsRoles = new AccountsRoles
+                    {
+                        AccountId = accounts.AccountId,
+                        RolesId = registerVM.RoleId
+                    };
+                    myContext.AccountsRoles.Add(accountsRoles);
+                    myContext.SaveChanges();
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            else //ada yang sama
+            {
+                return 3;
+            }
         }
     }
 }
