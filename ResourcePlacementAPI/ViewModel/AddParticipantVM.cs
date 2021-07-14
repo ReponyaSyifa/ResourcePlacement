dffd06acf3be506071006413a5e77f319a74da33
Syifa <85871262+ReponyaSyifa@users.noreply.github.com>
2021-07-14 12:10:13 +0700

    add AddParticipant

---
+++ ResourcePlacementAPI/ViewModel/AddParticipantVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourcePlacementAPI.ViewModel
+{
+    public enum Gender
+    {
+        Pria, Wanita
+    }
+    public class AddParticipantVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Grade { get; set; }
+        public string Status { get; set; }
+        public int ProjectsId { get; set; }
+    }
+}
