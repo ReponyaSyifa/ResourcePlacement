daa52dd932793932cc8551e8b59b6f24efc3145b
Syifa <85871262+ReponyaSyifa@users.noreply.github.com>
2021-07-15 16:23:56 +0700

    Add Dashboard Eksternal

---
+++ Client/Controllers/EksternalController.cs
@@ -0,0 +1,51 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class EksternalController : Controller
+    {
+        private readonly ILogger<EksternalController> _logger;
+
+        public EksternalController(ILogger<EksternalController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() //landing pange
+        {
+            return View();
+        }
+
+        public IActionResult Login() //landing pange
+        {
+            return View();
+        }
+        public IActionResult Eksternal() //landing pange
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard() //landing pange
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
