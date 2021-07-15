029989f82edf000ed331600bf7f915a569d96bd6
Syifa <85871262+ReponyaSyifa@users.noreply.github.com>
2021-07-15 13:36:56 +0700

    add Layouts

---
+++ Client/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() //landing pange
+        {
+            return View();
+        }
+
+        public IActionResult Login() //landing pange
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
