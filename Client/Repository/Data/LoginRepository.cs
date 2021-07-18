421a8d0080b122f633c4a443ffd3def6e58be937
Syifa <85871262+ReponyaSyifa@users.noreply.github.com>
2021-07-18 17:11:03 +0700

    test(2)

---
+++ Client/Repository/Data/LoginRepository.cs
@@ -0,0 +1,59 @@
+﻿using Client.Base;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using ResourcePlacementAPI.Models;
+using ResourcePlacementAPI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Repository.Data
+{
+    public class LoginRepository
+    {
+        private readonly Address address;
+        private readonly string request;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly HttpClient httpClient;
+        public LoginRepository(Address address, string request = "Account/") 
+        {
+            this.address = address;
+            this.request = request;
+            _contextAccessor = new HttpContextAccessor();
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address.link)
+            };
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", _contextAccessor.HttpContext.Session.GetString("Jwt"));
+        }
+
+        public async Task<JWTokenVM> Auth(LoginVM login)
+        {
+            JWTokenVM token = null;
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(login), Encoding.UTF8, "application/json");
+            var result = await httpClient.PostAsync(request + "login", content);
+
+            if (result.IsSuccessStatusCode)
+            {
+                string apiResponse = await result.Content.ReadAsStringAsync();
+                token = JsonConvert.DeserializeObject<JWTokenVM>(apiResponse);
+            }
+
+            return token;
+        }
+
+        public string JwtRole(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken result = tokenHandler.ReadJwtToken(token);
+
+            return result.Claims.FirstOrDefault(claim => claim.Type.Equals("role")).Value;
+        }
+    }
+}
